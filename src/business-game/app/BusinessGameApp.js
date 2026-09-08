cf9db71712c01ba8a808381b545bc8a8b0e04b2d
robertoiosifrbt-rgb <robertoiosifrbt@gmail.com>
2026-09-08 22:24:43 +0100

    Wire playable business vertical slice

---
+++ src/business-game/app/BusinessGameApp.js
@@ -0,0 +1,12 @@
+import { BusinessStore } from '../state/store.js'
+import { BusinessScene } from '../scene/BusinessScene.js'
+import { GameShell } from '../ui/GameShell.js'
+import '../ui/business-game.css'
+
+export class BusinessGameApp{
+ constructor(host){this.host=host;this.store=null;this.scene=null;this.ui=null}
+ start(){
+  this.host.innerHTML='';this.host.className='business-game';this.store=new BusinessStore();this.scene=new BusinessScene(this.host);this.ui=new GameShell(this.host,this.store);this.unsubscribe=this.store.subscribe(state=>this.scene.update(state));return this
+ }
+ destroy(){this.unsubscribe?.();this.ui?.destroy();this.scene?.destroy();this.host.innerHTML=''}
+}
