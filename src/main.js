bbec010ebe6c224d7d7a0ec427dab2d27ade8da6
robertoiosifrbt-rgb <robertoiosifrbt@gmail.com>
2026-09-09 11:52:08 +0100

    Publish V11 zero rebuild version

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import './game3d/dialogue-v5.css'
 import './game3d/palette-v6.css'
 import { VisualBusinessGame } from './game3d/VisualBusinessGame.js'
 
-export const APP_VERSION='6.3.0-city-districts'
+export const APP_VERSION='7.0.0-zero-rebuild'
 const mount=document.querySelector('#app')
 const game=new VisualBusinessGame(mount)
 try{await game.start()}catch(e){
