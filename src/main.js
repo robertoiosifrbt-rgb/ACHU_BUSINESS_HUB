b8e84b3d013f967ed6f5fb3255b91835db7297c2
robertoiosifrbt-rgb <robertoiosifrbt@gmail.com>
2026-09-09 07:41:51 +0100

    Bump version for Kenney plot layout fix

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import './game3d/dialogue-v5.css'
 import './game3d/palette-v6.css'
 import { VisualBusinessGame } from './game3d/VisualBusinessGame.js'
 
-export const APP_VERSION='5.2.0-kenney'
+export const APP_VERSION='5.2.1-layout-fix'
 const mount=document.querySelector('#app')
 const game=new VisualBusinessGame(mount)
 try{await game.start()}catch(e){
