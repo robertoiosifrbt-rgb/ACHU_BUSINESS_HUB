8e9e568dff30b9d150ba0d4a6c8bb7e089fe8c2f
robertoiosifrbt-rgb <robertoiosifrbt@gmail.com>
2026-09-09 16:57:00 +0100

    Bump app version for connected V16 layout

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import './game3d/dialogue-v5.css'
 import './game3d/palette-v6.css'
 import { VisualBusinessGame } from './game3d/VisualBusinessGame.js'
 
-export const APP_VERSION='11.0.0-v16-rebuild'
+export const APP_VERSION='11.1.0-v16-layout-fix'
 const mount=document.querySelector('#app')
 const game=new VisualBusinessGame(mount)
 try{await game.start()}catch(e){
