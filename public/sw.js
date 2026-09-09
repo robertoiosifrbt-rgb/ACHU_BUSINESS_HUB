850b345a258e6bc178756da7ae71e96b9ad8efa2
robertoiosifrbt-rgb <robertoiosifrbt@gmail.com>
2026-09-09 01:12:44 +0100

    Bust service worker cache for gameplay fixes

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='achu-business-hub-v4.1.1-4x';
+const CACHE='achu-business-hub-v4.2.0-4x';
 const BASE='/ACHU_BUSINESS_HUB/';
 const SHELL=[BASE,BASE+'manifest.webmanifest',BASE+'version.json',BASE+'icons/icon-192.png',BASE+'icons/icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL).catch(()=>{})));self.skipWaiting()});
