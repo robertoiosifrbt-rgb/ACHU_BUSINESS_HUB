b60dbefafff4b026c33bb7b1ebbd4ba68f60252c
robertoiosifrbt-rgb <robertoiosifrbt@gmail.com>
2026-09-09 01:20:14 +0100

    Bump app version for real crew fix

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import './game3d/4x.css'
 import './game3d/story.css'
 import { Business4XGame } from './game3d/Business4XGame.js'
 
-export const APP_VERSION='4.2.0-4x'
+export const APP_VERSION='4.2.1-4x'
 const mount=document.querySelector('#app')
 const game=new Business4XGame(mount)
 try{await game.start()}catch(e){
