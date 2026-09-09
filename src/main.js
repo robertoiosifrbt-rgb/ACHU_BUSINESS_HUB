7990afaf0a2900ecc6677cab28cf420a5215bc1d
robertoiosifrbt-rgb <robertoiosifrbt@gmail.com>
2026-09-09 15:18:08 +0100

    Bump live game for V15 city

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import './game3d/dialogue-v5.css'
 import './game3d/palette-v6.css'
 import { VisualBusinessGame } from './game3d/VisualBusinessGame.js'
 
-export const APP_VERSION='10.1.0-v15-live'
+export const APP_VERSION='10.2.0-v15-city'
 const mount=document.querySelector('#app')
 const game=new VisualBusinessGame(mount)
 try{await game.start()}catch(e){
