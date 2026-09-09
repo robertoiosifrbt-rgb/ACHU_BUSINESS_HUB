3d8abf1036db9d04bc769904d433eeb3b96ad684
robertoiosifrbt-rgb <robertoiosifrbt@gmail.com>
2026-09-09 14:12:47 +0100

    Start Unity migration runtime on main

---
+++ unity/Assets/ACHU/Runtime/AchuIds.cs
@@ -0,0 +1,19 @@
+namespace Achu.BusinessHub
+{
+    public static class AchuIds
+    {
+        public const string Headquarters = "furnace";
+        public const string ClientCentre = "shelter";
+        public const string SupplyDepot = "sawmill";
+        public const string SalesOffice = "huntersHut";
+        public const string MarketingStudio = "coalMine";
+        public const string CentralWarehouse = "storehouse";
+        public const string FieldAcademy = "infantryCamp";
+        public const string QualityCentre = "infirmary";
+        public const string RecruitmentHub = "ironMine";
+        public const string FleetDepot = "lancerCamp";
+        public const string PartnershipOffice = "embassy";
+        public const string SpecialistUnit = "marksmanCamp";
+        public const string InnovationLab = "researchCenter";
+    }
+}
