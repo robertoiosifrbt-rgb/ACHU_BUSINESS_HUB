e18041ddaff5a558bf03e7bd6ccad6740b4d9c84
robertoiosifrbt-rgb <robertoiosifrbt@gmail.com>
2026-09-09 10:31:26 +0100

    Expose creative 3D city app version

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import './game3d/dialogue-v5.css'
 import './game3d/palette-v6.css'
 import { VisualBusinessGame } from './game3d/VisualBusinessGame.js'
 
-export const APP_VERSION='6.1.0-city-street-graph'
+export const APP_VERSION='6.2.0-creative-3d-city'
 const mount=document.querySelector('#app')
 const game=new VisualBusinessGame(mount)
 try{await game.start()}catch(e){
